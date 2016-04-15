9954dc1fa136411f28e4e4594e1673d6615ca57a
Hongkai Liu <hongkailiu@users.noreply.github.com>
2016-04-15 16:08:41 -0400

    use getter instead of data

--- src/main/java/com/example/devtoolindex/db/entity/IPStatEntity.java
+++ src/main/java/com/example/devtoolindex/db/entity/IPStatEntity.java
@@ -1,35 +1,59 @@
 package com.example.devtoolindex.db.entity;
 
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.Date;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by hongkailiu on 2016-04-11.
  */
 @Document(collection = "ipstat")
-@EqualsAndHashCode(callSuper=false)
-@Data public class IPStatEntity extends BaseEntity {
+public class IPStatEntity extends BaseEntity {
 
     public static final String VISIT_DATE_NAME ="visit_date";
     public static final long TTL = TimeUnit.DAYS.toSeconds(30);
 
-    private String ip;
+    @Getter private String uuid;
+    @Setter @Getter private String ip;
     @Field(VISIT_DATE_NAME)
-    private Date visitDate;
-    private String path;
+    @Setter @Getter private Date visitDate;
+    @Setter @Getter private String path;
 
     public IPStatEntity(String ip, Date visitDate,  String path){
+        init();
         this.ip = ip;
         this.visitDate = visitDate;
         this.path = path;
     }
 
     public IPStatEntity(){
+        init();
+    }
+
+    private void init(){
+        this.uuid = UUID.randomUUID().toString();
+    }
+
+    @Override public int hashCode() {
+        return new HashCodeBuilder().append(uuid).toHashCode();
+    }
+
+    @Override public boolean equals(final Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != getClass()) {
+            return false;
+        }
+        IPStatEntity other = (IPStatEntity) obj;
+        return new EqualsBuilder().append(uuid, other.uuid).isEquals();
     }
 }
