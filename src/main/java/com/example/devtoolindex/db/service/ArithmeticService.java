c51f297944b7ab09ea4cd16c005859e16845fcd4
hongkailiu <hongkailiu@users.noreply.github.com>
2016-04-15 21:04:00 -0400

    add arithmetic service

---
+++ src/main/java/com/example/devtoolindex/db/service/ArithmeticService.java
@@ -0,0 +1,15 @@
+package com.example.devtoolindex.db.service;
+
+/**
+ * Created by hongkailiu on 2016-04-15.
+ */
+public interface ArithmeticService {
+    /**
+     * calculate i plus j
+     *
+     * @param i int i
+     * @param j int j
+     * @return the result of i plus j
+     */
+    int add(int i, int j);
+}
---
+++ src/main/java/com/example/devtoolindex/db/service/ArithmeticServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.devtoolindex.db.service;
+
+/**
+ * Created by hongkailiu on 2016-04-15.
+ */
+public class ArithmeticServiceImpl implements ArithmeticService {
+    @Override public int add(int i, int j) {
+        return i + j;
+    }
+}
